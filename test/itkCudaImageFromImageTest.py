455173a72e06bae6272d3d91162dd6f7661e7943
Axel Garcia <axel.garcia1997@gmail.com>
2025-07-11 13:22:28 +0200

    ENH: Run Python tests by the CI

--- test/itkCudaImageFromImageTest.py
+++ test/itkCudaImageFromImageTest.py
@@ -1,28 +1,29 @@
 import itk
 
-# Define image type (2D float Image)
-PixelType = itk.F
-Dimension = 2
-ImageType = itk.Image[PixelType, Dimension]
+def test_cuda_image_from_image():
+    # Define image type (2D float Image)
+    PixelType = itk.F
+    Dimension = 2
+    ImageType = itk.Image[PixelType, Dimension]
 
-# Set size and region
-size = itk.Size[Dimension]()
-size.Fill(256)
+    # Set size and region
+    size = itk.Size[Dimension]()
+    size.Fill(256)
 
-region = itk.ImageRegion[Dimension]()
-region.SetSize(size)
+    region = itk.ImageRegion[Dimension]()
+    region.SetSize(size)
 
-# Create and allocate the CUDA image
-image = ImageType.New()
-image.SetRegions(region)
-image.Allocate()
+    # Create and allocate the CUDA image
+    image = ImageType.New()
+    image.SetRegions(region)
+    image.Allocate()
 
-# Fill image with a constant value
-fill_value = 2.0
-image.FillBuffer(fill_value)
+    # Fill image with a constant value
+    fill_value = 2.0
+    image.FillBuffer(fill_value)
 
-# Convert image
-cuda_img = itk.cuda_image_from_image(image)
+    # Convert image
+    cuda_img = itk.cuda_image_from_image(image)
 
-# Check results
-assert isinstance(cuda_img, itk.CudaImage), "Failed to convert to CudaImage"
+    # Check results
+    assert isinstance(cuda_img, itk.CudaImage), "Failed to convert to CudaImage"
